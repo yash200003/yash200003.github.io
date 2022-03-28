1f4aef1212ab8f5b7205d644aeb969a468c64dd5
yash200003 <79564683+yash200003@users.noreply.github.com>
2022-03-28 14:52:43 +0400

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -30,7 +30,7 @@
     //
     // A definition query filters what was first retrieved from the SPL feature service
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById( '17fd0055437-layer-26' ); 
+        var svcLyr = gMyWebmap.findLayerById( '17fd0264f29-layer-16' ); 
         console.log( "Layer is");
         console.log( svcLyr);
 
