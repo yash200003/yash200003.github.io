ed1adffb800ac51d10df927c9dcad4ebf6ee7a55
yash200003 <79564683+yash200003@users.noreply.github.com>
2022-04-04 11:15:21 +0400

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -40,7 +40,7 @@
         // only execute when the sublayer is loaded. Note this is asynchronous
         // so it may be skipped over during execution and be executed after exiting this function
         svcLyr.when(function() {
-            gMyLyr =svcLyr.findSublayerById('17ff35af194-layer-6');
+            gMyLyr =svcLyr.findSublayerById('17ff368b02e-object-3');
             console.log("Sublayer loaded...");
             console.log( "Sublayer is");
             console.log( gMyLyr);
