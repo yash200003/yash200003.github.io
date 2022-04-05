b38375c804cf4f65f4728b40ac8ac47d02a94e84
yash200003 <79564683+yash200003@users.noreply.github.com>
2022-04-05 14:16:25 +0400

    Update geomap_component.js

--- geomap_component.js
+++ geomap_component.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
     let template = document.createElement("template");
-    var layer;
+    const layer;
     var gPassedServiceType; // holds passed in guarantee of service - set in onCustomWidgetBeforeUpdate()
     var gPassedPortalURL; //ESRI Portal URL
     var gPassedAPIkey; //ESRI JS api key
@@ -80,27 +80,25 @@
     </div>
   </body>
     `;
-    
+
     // this function takes the passed in servicelevel and issues a definition query
     // to filter service location geometries
     //
     // A definition query filters what was first retrieved from the SPL feature service
-    
 
     // process the definition query on the passed in SPL feature sublayer
-   
 
     class Map extends HTMLElement {
         constructor() {
             super();
-            
+
             //this._shadowRoot = this.attachShadow({mode: "open"});
             this.appendChild(template.content.cloneNode(true));
             this._props = {};
             let that = this;
 
             require(["esri/Map", "esri/views/MapView", "esri/layers/MapImageLayer"], (Map, MapView, MapImageLayer) => {
-        /*
+                /*
                 // set portal and API Key
                 esriConfig.portalUrl = gPassedPortalURL
 
@@ -112,163 +110,142 @@
                     url: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World"
                 });
                 */
-                
+
                 //new code
-                
+
                 const renderer = {
-          type: "simple", // autocasts as new SimpleRenderer()
-          symbol: {
-            type: "simple-line", // autocasts as new SimpleLineSymbol()
-            color: [255, 255, 255, 0.5],
-            width: 0.75,
-            style: "long-dash-dot-dot"
-          }
-        };
-                
+                    type: "simple", // autocasts as new SimpleRenderer()
+                    symbol: {
+                        type: "simple-line", // autocasts as new SimpleLineSymbol()
+                        color: [255, 255, 255, 0.5],
+                        width: 0.75,
+                        style: "long-dash-dot-dot",
+                    },
+                };
+
                 layer = new MapImageLayer({
-          url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",
-          sublayers: [
-            {
-              id: 3,
-              visible: false
-            },
-            {
-              id: 4,
-              visible: false,
-              title: "Railroads",
-              renderer: renderer,
-              source: {
-                // indicates the source of the sublayer is a dynamic data layer
-                type: "data-layer",
-                // this object defines the data source of the layer
-                // in this case it's a feature class table from a file geodatabase
-                dataSource: {
-                  type: "table",
-                  // workspace name
-                  workspaceId: "MyDatabaseWorkspaceIDSSR2",
-                  // table name
-                  dataSourceName: "ss6.gdb.Railroads"
-                }
-              }
-            },
-            {
-              id: 2,
-              visible: false
-            },
-            {
-              id: 1,
-              visible: false
-            }
-          ]
-        });
-                
-                
-            const map = new Map({
-          basemap: "dark-gray-vector",
-          layers: [layer]
-        });
+                    url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",
+                    sublayers: [
+                        {
+                            id: 3,
+                            visible: false,
+                        },
+                        {
+                            id: 4,
+                            visible: false,
+                            title: "Railroads",
+                            renderer: renderer,
+                            source: {
+                                // indicates the source of the sublayer is a dynamic data layer
+                                type: "data-layer",
+                                // this object defines the data source of the layer
+                                // in this case it's a feature class table from a file geodatabase
+                                dataSource: {
+                                    type: "table",
+                                    // workspace name
+                                    workspaceId: "MyDatabaseWorkspaceIDSSR2",
+                                    // table name
+                                    dataSourceName: "ss6.gdb.Railroads",
+                                },
+                            },
+                        },
+                        {
+                            id: 2,
+                            visible: false,
+                        },
+                        {
+                            id: 1,
+                            visible: false,
+                        },
+                    ],
+                });
 
-        const view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 3,
-          center: [-99, 39]
-        });
+                const map = new Map({
+                    basemap: "dark-gray-vector",
+                    layers: [layer],
+                });
 
-        /*****************************************************************
-         * Wait for Layer to load and update the page to refelect which
-         * layers are visible in the Map Service.
-         *****************************************************************/
-        layer.when(() => {
-          layer.sublayers.map((sublayer) => {
-            const id = sublayer.id;
-            const visible = sublayer.visible;
-            const node = document.querySelector(".sublayers-item[data-id='" + id + "']");
-            if (visible) {
-              node.classList.add("visible-layer");
-            }
-          });
-        });
+                const view = new MapView({
+                    container: "viewDiv",
+                    map: map,
+                    zoom: 3,
+                    center: [-99, 39],
+                });
 
-        
-                
-                
-                
-        
-
+                /*****************************************************************
+                 * Wait for Layer to load and update the page to refelect which
+                 * layers are visible in the Map Service.
+                 *****************************************************************/
+                layer.when(() => {
+                    layer.sublayers.map((sublayer) => {
+                        const id = sublayer.id;
+                        const visible = sublayer.visible;
+                        const node = document.querySelector(".sublayers-item[data-id='" + id + "']");
+                        if (visible) {
+                            node.classList.add("visible-layer");
+                        }
+                    });
+                });
             }); // end of require()
-        } // end of constructor()    
+        } // end of constructor()
 
         getSelection() {
             return this._currentSelection;
         }
 
-        
-
-        onCustomWidgetAfterUpdate(changedProperties) 
-        {
+        onCustomWidgetAfterUpdate(changedProperties) {
             if ("servicelevel" in changedProperties) {
                 this.$servicelevel = changedProperties["servicelevel"];
                 gPassedServiceType = this.$servicelevel; // place passed in value into global
-            
-            
-            /*****************************************************************
-         * Listen for when buttons on the page have been clicked to turn
-         * layers on and off in the Map Service.
-         *****************************************************************/
-        
-          
-            const sublayer = layer.findSublayerById(parseInt(gPassedServiceType));
-            const node = document.querySelector(".sublayers-item[data-id='" + gPassedServiceType + "']");
-            sublayer.visible = !sublayer.visible;
-            node.classList.toggle("visible-layer");
+
+                /*****************************************************************
+                 * Listen for when buttons on the page have been clicked to turn
+                 * layers on and off in the Map Service.
+                 *****************************************************************/
+
+                const sublayer = layer.findSublayerById(parseInt(gPassedServiceType));
+                const node = document.querySelector(".sublayers-item[data-id='" + gPassedServiceType + "']");
+                sublayer.visible = !sublayer.visible;
+                node.classList.toggle("visible-layer");
             }
             gPassedServiceType = this.$servicelevel; // place passed in value into global
-            
-            
-      
- 
-
-       
-
-  
         }
     } // end of class
 
     let scriptSrc = "https://js.arcgis.com/4.18/";
-    let onScriptLoaded = function() {
+    let onScriptLoaded = function () {
         customElements.define("com-sap-custom-geomap", Map);
     };
 
     //SHARED FUNCTION: reuse between widgets
     //function(src, callback) {
     let customElementScripts = window.sessionStorage.getItem("customElementScripts") || [];
-    let scriptStatus = customElementScripts.find(function(element) {
+    let scriptStatus = customElementScripts.find(function (element) {
         return element.src == scriptSrc;
     });
 
     if (scriptStatus) {
-        if(scriptStatus.status == "ready") {
+        if (scriptStatus.status == "ready") {
             onScriptLoaded();
         } else {
             scriptStatus.callbacks.push(onScriptLoaded);
         }
     } else {
         let scriptObject = {
-            "src": scriptSrc,
-            "status": "loading",
-            "callbacks": [onScriptLoaded]
-        }
+            src: scriptSrc,
+            status: "loading",
+            callbacks: [onScriptLoaded],
+        };
         customElementScripts.push(scriptObject);
         var script = document.createElement("script");
         script.type = "text/javascript";
         script.src = scriptSrc;
-        script.onload = function(){
+        script.onload = function () {
             scriptObject.status = "ready";
             scriptObject.callbacks.forEach((callbackFn) => callbackFn.call());
         };
         document.head.appendChild(script);
     }
 
-//END SHARED FUNCTION
+    //END SHARED FUNCTION
 })(); // end of class
