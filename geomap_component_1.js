27cb9d93778e0416d03df2e28ea90f15e23ae506
yash200003 <79564683+yash200003@users.noreply.github.com>
2022-04-08 14:49:17 +0400

    Add files via upload

---
+++ geomap_component_1.js
@@ -0,0 +1,76 @@
+(function() {
+    let template = document.createElement("template");
+   
+    template.innerHTML = `
+        <link rel="stylesheet" href="https://js.arcgis.com/4.18/esri/themes/light/main.css">
+        <style>
+        #mapview {
+            width: 100%;
+            height: 100%;
+        }
+        #timeSlider {
+            position: absolute;
+            left: 5%;
+            right: 15%;
+            bottom: 20px;
+        }
+        </style>
+        <div id='mapview'></div>
+        <div id='timeSlider'></div>
+    `;
+    
+    
+
+    class Map extends HTMLElement {
+        constructor() {
+            super();
+            
+            //this._shadowRoot = this.attachShadow({mode: "open"});
+            this.appendChild(template.content.cloneNode(true));
+            this._props = {};
+            let that = this;
+
+            
+            
+        } // end of constructor()    
+
+        
+    } // end of class
+
+    let scriptSrc = "https://js.arcgis.com/4.18/"
+    let onScriptLoaded = function() {
+        customElements.define("com-sap-custom-geomap1", Map);
+    }
+
+    //SHARED FUNCTION: reuse between widgets
+    //function(src, callback) {
+    let customElementScripts = window.sessionStorage.getItem("customElementScripts") || [];
+    let scriptStatus = customElementScripts.find(function(element) {
+        return element.src == scriptSrc;
+    });
+
+    if (scriptStatus) {
+        if(scriptStatus.status == "ready") {
+            onScriptLoaded();
+        } else {
+            scriptStatus.callbacks.push(onScriptLoaded);
+        }
+    } else {
+        let scriptObject = {
+            "src": scriptSrc,
+            "status": "loading",
+            "callbacks": [onScriptLoaded]
+        }
+        customElementScripts.push(scriptObject);
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = scriptSrc;
+        script.onload = function(){
+            scriptObject.status = "ready";
+            scriptObject.callbacks.forEach((callbackFn) => callbackFn.call());
+        };
+        document.head.appendChild(script);
+    }
+
+//END SHARED FUNCTION
+})(); // end of class
